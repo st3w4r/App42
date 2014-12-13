7263108eede80143aa4c598c4f4e305019e7c64e
Yanaël <b.yanael@gmail.com>
2014-12-13 22:06:00 +0100

    Initial commit

---
+++ App42/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App42
+//
+//  Created by Yanaël Barbier on 13/12/2014.
+//  Copyright (c) 2014 Yanael Barbier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
